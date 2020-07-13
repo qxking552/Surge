223d3ac399406c9d6efe6f4b2ba9d47a52123a0d
qxking552 <yghack@gmail.com>
2020-07-13 22:48:39 +0800

    Create caiyun.js

---
+++ caiyun.js
@@ -0,0 +1,8 @@
+let obj = JSON.parse($response.body);
+
+    obj.result.vip_expired_at = 4075798974,
+    obj.result.is_xy_vip = true,
+    obj.result.xy_vip_expire = 0,
+    obj.result.is_vip = true,
+    obj.result.enabled =  true,
+    obj.expired_at = 4075798974
